c20171ea77d9aad120eccffece8d039f6d3d7776
wl1105 <1031270528@qq.com>
2019-04-15 21:22:01 +0800

    上传Profile我的部分

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,5 +1,229 @@
 <template>
-  <div>
-    这是我的页面
+  <div class="profile-1vcJW">
+    <!-- 头部 -->
+    <section>
+      <div class="profile-3g0uv">
+        <div class="index-18Ili">
+          <span></span>
+        </div>
+        <div class="profile-xfCcC">
+          <p class="profile-1_mtk">
+            <span>4ee6228a050</span>
+          </p>
+          <p class="profile-1UP72">
+            <i class="iconfont icon-shouji"></i>
+            <span>135****8893</span>
+          </p>
+        </div>
+        <i class="profile-2XuMq iconfont icon-right"></i>
+      </div>
+    </section>
+    <!-- 红包金币 -->
+    <div class="index-1G7HV">
+      <a href="#" class="index-1ryAh">
+        <p>
+          <span class="index-2FmrF" style="color: rgb(255, 95, 62);">0</span><span class="index-2V-Hh"
+            style="color: rgb(255, 95, 62);">个</span>
+        </p>
+        <p class="index-3S6cZ">红包</p>
+      </a>
+      <a href="#" class="index-1ryAh">
+        <p>
+          <span class="index-2FmrF" style="color: rgb(106, 194, 11);">0</span><span class="index-2V-Hh"
+            style="color: rgb(106, 194, 11);">个</span>
+        </p>
+        <p class="index-3S6cZ">金币</p>
+      </a>
+    </div>
+    <!-- 我的地址 -->
+    <section class="profile-1reTe">
+      <a href="#" class="index-2MEEn">
+        <i class="iconfont icon-dizhi" style="color:rgb(74, 165, 240);"></i>
+        <div class="index-yydpu">我的地址
+          <i class="iconfont icon-right"></i>
+        </div>
+      </a>
+    </section>
+    <!-- 金币商城 -->
+    <section class="profile-1reTe">
+      <a href="#" class="index-2MEEn">
+        <i class="iconfont icon-shangcheng" style="color:rgb(148, 217, 74)"></i>
+        <div class="index-yydpu">金币商城
+          <i class="iconfont icon-right"></i>
+        </div>
+      </a>
+      <a href="#" class="index-2MEEn">
+        <i class="iconfont icon-liwu" style="color:rgb(252, 123, 83)"></i>
+        <div class="index-yydpu">分享拿10元现金
+          <i class="iconfont icon-right"></i>
+        </div>
+      </a>
+    </section>
+    <!-- 我的客服 -->
+    <section class="profile-1reTe">
+      <a href="#" class="index-2MEEn">
+        <i class="iconfont icon-erji" style="color:rgb(74, 165, 240)"></i>
+        <div class="index-yydpu">我的客服
+          <i class="iconfont icon-right"></i>
+        </div>
+      </a>
+      <a href="#" class="index-2MEEn">
+        <i class="iconfont icon-eliaomo" style="color:rgb(74, 165, 240)"></i>
+        <div class="index-yydpu">下载饿了么APP
+          <i class="iconfont icon-right"></i>
+        </div>
+      </a>
+      <a href="#" class="index-2MEEn">
+        <i class="iconfont icon-guize" style="color:rgb(74, 165, 240)"></i>
+        <div class="index-yydpu">规则中心
+          <i class="iconfont icon-right"></i>
+        </div>
+      </a>
+    </section>
+    <!-- 隐私政策 -->
+    <div class="profile-2dyk_">
+      <a
+        href="//h5.ele.me/service/agreement/#HEAEDER_SHOW=1&initTitle=%E9%9A%90%E7%A7%81%E6%94%BF%E7%AD%96&key=ruleQue50">隐私政策</a>
+    </div>
   </div>
 </template>
+
+<style>
+  /* 头部 */
+body{
+  background-color: #f5f5f5;
+}
+a{
+  text-decoration: none;
+  cursor: pointer;
+}
+.profile-3g0uv{
+  height: 1.1rem; 
+  display: flex;
+  background-image: linear-gradient(90deg,#0af,#0085ff);
+  padding: 6.666667vw 4vw;
+  align-items: center;
+}
+.profile-3g0uv .index-18Ili{
+  width: .6rem;
+  height: .6rem;
+  border-radius: 50%;
+}
+.profile-3g0uv .index-18Ili span {
+  display: block;
+  width: .6rem;
+  height: .6rem;
+  border-radius: 50%;
+  background-position: 0px 2.4rem;
+  background-size: .6rem;
+  background: url(//shadow.elemecdn.com/faas/h5/static/sprite.3ffb5d8.png);
+}
+.profile-3g0uv .profile-xfCcC{
+  overflow: hidden;
+  margin-left: 4.8vw;
+  flex-grow: 1;
+}
+.profile-3g0uv .profile-xfCcC p {
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+.profile-3g0uv .profile-xfCcC .profile-1_mtk{
+  font-size: .2rem;
+  font-weight: bold;
+  color: #fff;
+  margin-bottom: 1.065vw;
+  display: flex;
+  align-items: center;
+}
+.profile-3g0uv .profile-xfCcC .profile-1UP72{
+  font-size: .12rem;
+  color: #fff;
+  display: flex;
+  align-items: center;
+}
+.profile-1UP72 .icon-shouji{  
+  font-size: .12rem;
+}
+.profile-3g0uv .profile-2XuMq{
+  color: #fff;
+  font-size: .12rem;
+}
+/* 红包金币 */
+.index-1G7HV{
+  width: 100%;
+  height: .84rem;
+  display: table;
+  border-bottom: 1px solid #ddd;
+  background-color: #fff;
+}
+.index-1G7HV .index-1ryAh{
+  display: table-cell;
+  width: 33.333%;
+  vertical-align: middle;
+  text-align: center;
+}
+.index-1G7HV>a:nth-child(1){
+  border-right: 1px solid #ddd;
+  display: table-cell;
+}
+.index-1G7HV .index-2FmrF{
+  font-size: .22rem;
+  line-height: 6.933333vw;
+  font-weight: 700;
+}
+.index-1G7HV .index-3S6cZ{
+  margin-top: 1.333333vw;
+  font-size: .12rem;
+  line-height: 3.733333vw;
+  color: #666;
+  font-weight: 700;
+}
+/* 我的地址 */
+.profile-1reTe{
+  margin-top: 2.666667vw;
+  border-top: 1px solid #ddd;
+  border-bottom: 1px solid #ddd;
+  font-size: .05rem;
+  line-height: .45rem;
+  background: #fff;
+}
+.profile-1reTe .index-2MEEn{
+  font-size: .16rem;
+  color: #333;
+  display: flex;
+  align-items: center;
+  padding-left: .12rem;
+}
+.profile-1reTe .index-2MEEn .iconfont{
+  font-size: .2rem;
+  margin-right: 2.667vw;
+}
+.profile-1reTe .index-2MEEn .index-yydpu{
+  margin-right: .05rem;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+}
+.index-2MEEn:not(:last-child) .index-yydpu {
+  border-bottom: 1px solid #eee;
+}
+.profile-1reTe .index-2MEEn .icon-right{
+	font-size: .12rem;
+	margin-right: 1vw;
+  color: #bbb;
+} 
+/* 隐私政策 */
+.profile-2dyk_{
+  text-align: center;
+  margin: .613333rem auto;
+  margin: 6.133333vw auto;
+  color:#4da6f0;
+  font-size: .16rem;
+}
+.profile-2dyk_>a {
+  outline: none;
+  color: inherit;
+}
+</style>
+
