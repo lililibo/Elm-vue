73af7f6096d5b3d71d559ffc86d936283762c6a6
wl1105 <1031270528@qq.com>
2019-04-19 09:51:16 +0800

    解决vant引入的冲突

--- src/main.js
+++ src/main.js
@@ -4,23 +4,10 @@ import router from './router'
 import store from './store/index'
 import 'vant/lib/button/style';
 
-import { Collapse, CollapseItem, CouponCell, CouponList, Card, Popup, GoodsAction, GoodsActionBigBtn, GoodsActionMiniBtn, Button  } from 'vant';
+import { Collapse, AddressList, NavBar, CollapseItem, CouponCell, CouponList, Card, Popup, GoodsAction, GoodsActionBigBtn, GoodsActionMiniBtn, Button, Cell, CellGroup  } from 'vant';
 
-<<<<<<< HEAD
-Vue.use(Collapse).use(CollapseItem).use(CouponCell).use(CouponList);
-import { Cell, CellGroup } from 'vant';
-Vue.use(Cell).use(CellGroup)
-=======
-Vue.use(Collapse).use(CollapseItem).use(CouponCell).use(CouponList).use(Card).use(Popup).use(GoodsAction).use(GoodsActionBigBtn).use(GoodsActionMiniBtn).use(Button);
->>>>>>> 8b31b5f908279d11a7e4f8b8e869343539e04a12
+Vue.use(Collapse).use(CollapseItem).use(CouponCell).use(CouponList).use(Card).use(Popup).use(GoodsAction).use(GoodsActionBigBtn).use(GoodsActionMiniBtn).use(Button).use(Cell).use(CellGroup).use(AddressList).use(NavBar);
 
-import { AddressList,NavBar } from 'vant'
-Vue.use(AddressList).use(NavBar)
-
-import { Button } from 'vant';
-
-Vue.use(Button);
-//按钮
 
 router.beforeEach((to, from, next) => {
   /* 路由发生变化修改页面title */
